7a5e188713af29e9273e18ba10440141ba8203cf
Lucaso1424 <lucaspadilla648@gmail.com>
2021-03-16 23:08:30 +0100

    lph - comentaris i responsive

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ function llamadaJSON() {
         url: "https://analisi.transparenciacatalunya.cat/resource/yqes-rqmp.json",
         type: "GET",
         data: {
+            // PONEMOS EN EL LIMITE 1000 ENTRADAS
             "$limit": 1000,
             "$$app_token": "EIsAhk53hwzCPMmgS0dA44ulq",
             // APLICAMOS UN FILTRO DE LA CLAVE DEL JSON
@@ -21,6 +22,7 @@ function llamadaJSON() {
     }).done(function (data) {
         alert("Se han recogido " + data.length + " datos de la base de datos!");
         console.log(data);
+        // LLAMAMOS A LA FUNCIÓN PASANDOLE EL DATA (EL JSON)
         imprimirTabla(data);
     });
 }
@@ -30,6 +32,7 @@ function imprimirTabla(data) {
     tabla.setAttribute("class", "tabla");
     tabla.setAttribute("id", "imprimir_tabla");
 
+    // RECORREMOS EL FOR POR CADA VALOR DEL JSON
     for (let i = 0; i < data.length; i++) {
         let col = document.createElement("tr");
         col.setAttribute("class", "columna");
