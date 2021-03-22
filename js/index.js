f52838536830fefe6953164d5336faa422659e66
Lucaso1424 <lucaspadilla648@gmail.com>
2021-03-22 23:25:10 +0100

    lph - GoogleChart chart sectors finalitzats

--- js/index.js
+++ js/index.js
@@ -18,11 +18,11 @@ function llamadaJSON() {
             // PONEMOS EN EL LIMITE 1000 ENTRADAS
             "$limit": 1000,
             "$$app_token": "EIsAhk53hwzCPMmgS0dA44ulq",
-            // APLICAMOS UN FILTRO DE LA CLAVE DEL JSON
-            "sector": "Industrial"
+            // APLICAMOS UN FILTRO DE LA CLAVE DEL JSON (LO DEJAMOS COMENTADO PARA LAS GRÁFICAS DEL GOOGLE CHART)
+            // "sector": "Industrial"
         }
     }).done(function (data) {
-        alert("Se han recogido " + data.length + " datos del JSON!");
+        alert("S'han recollit " + data.length + " dades/entrades del JSON!");
         console.log(data);
         // LLAMAMOS A LA FUNCIÓN PASANDOLE EL DATA (EL JSON)
         imprimirTabla(data);
@@ -31,7 +31,8 @@ function llamadaJSON() {
         google.charts.load('current', {
             'packages': ['corechart']
         });
-        // CARGAMOS UN CALLBACK PARA CUANDO LAS APIS DE GOOGLE ESTÉN CARGADAS
+        // CARGAMOS UN CALLBACK PARA CUANDO LAS APIS DE GOOGLE ESTÉN CARGADAS, LO LANZAMOS EN UNA FUNCIÓN ANÓNIMA
+        // PARA PASAR COMO PARÁMETRO EL data (DATOS DEL JSON)
         google.charts.setOnLoadCallback(function () {
             drawChart(data)
         });
@@ -76,28 +77,45 @@ function ocultarBoton() {
 }
 
 function drawChart(data) {
-    // DECLARAMOS LOS LET DE LOS VALORES DE LOS GRÁFICOS
+    // DECLARAMOS LOS LET DE LOS PROPIOS SECTORES Y LOS INICIALIZAMOS EN NULL
     let sector1 = null;
     let sector2 = null;
+    let sector3 = null;
+    let sector4 = null;
+    let sector5 = null;
 
-    console.log(data[0].sector)
-    // RECORREMOS EL FOR POR CADA VALOR DEL JSON
+    // RECORREMOS EL FOR POR CADA VALOR DEL JSON, CON EL ATRIBUTO DE sector DEL PROPIO JSON, Y LO SUMAMOS AL CONTADOR
     for (let i = 0; i < data.length; i++) {
         if (data[i].sector == "Industrial") {
             sector1++;
-        } 
-        console.log(sector1);
+        }
+        if (data[i].sector == "Alimentació") {
+            sector2++;
+        }
+        if (data[i].sector == "Financer") {
+            sector3++;
+        }
+        if (data[i].sector == "Farmacèutic") {
+            sector4++;
+        }
+        if (data[i].sector == "Farmacèutic") {
+            sector5++;
+        }
     }
 
     // CREAMOS LA VARIABLE DATA PARA UTILIZAR EL DATA_TABLE
     var data_table = google.visualization.arrayToDataTable([
-        ['Chart', 'Chart de sectors industrial'],
-        ['Sector industrial', sector1]
+        ['Chart', 'Chart de sectors industrial'], 
+        ['Sector Industrial', sector1], 
+        ['Sector Alimentació', sector2], 
+        ['Sector Financer', sector3],
+        ['Sector Farmacèutic', sector4],
+        ['Sector Educació', sector5],
     ]);
 
     // DEFINIMOS LOS ATRIBUTOS DE LA GRÁFICA
     var options = {
-        'title': 'Gràfic amb els sector de Serveis',
+        'title': 'Gràfic dels Sectors',
         'width': 400,
         'height': 300,
         is3D: true
