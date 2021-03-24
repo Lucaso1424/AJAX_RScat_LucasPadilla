abf7512474941edf673afa16fb4c6dd9918a35b8
Lucaso1424 <lucaspadilla648@gmail.com>
2021-03-24 02:31:45 +0100

    lph - evento dbclick añade marcador

--- js/map.js
+++ js/map.js
@@ -14,9 +14,19 @@ function cargarMapaLeaflet() {
 
     let marker = L.marker([41.39210, 2.170840]).addTo(myMap);
 
+    // DESHABILITAMOS EL ZOOM CON DBLCLICK
     myMap.doubleClickZoom.disable();
-    myMap.on('dbclick', e => {
-        let latLng = myMap.mouseEventToLatLng(e.originalEvent);
-        L.marker([latLng.lat, latLng.lng]).addTo(myMap);
+    myMap.on('dblclick', function (e) {
+        // SELECCIONAMOS UN NUEVO MARCADOR CON EL marker, AÑADIMOS LA LATITUD Y LONGITUD 
+        // EN LA VARIABLE DE myMap (nuestro mapa creado anteriormente)
+        let new_marker = L.marker(e.latlng).addTo(myMap);
+        let markerpopup = L.popup({
+        });
+       // AÑADIMOS EL POPUP Y SETTEAMOS LA LATITUD Y LONGITUD CON LA FUNCIÓN setLatLng
+        markerpopup.setLatLng(e.latlng);
+        // AÑADIMOS EL CONTENIDO DEL POPUP CON LA FUNCIÓN DE setContent
+        markerpopup.setContent("Popup");     
+        // MARCAMOS EL ENLACE DEL POPUP
+        marker.bindPopup(markerpopup);
     });
 }
