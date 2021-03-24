239c7ec499b18666c267f3668901d7941da911d2
Lucaso1424 <lucaspadilla648@gmail.com>
2021-03-24 16:50:44 +0100

    lph - bucle con todos los markers del json anclados

--- js/map.js
+++ js/map.js
@@ -1,39 +1,30 @@
 function cargarMapaLeaflet(data) {
     const tilesProvider = "https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png";
 
-    // CREAMOS LA LATITUD Y LONGITUD PARA GUARDAR LA INFO DEL JSON
-    let latitud_mapa;
-    let longitud_mapa;
+    // CREAMOS UN ARRAY PARA GUARDAR LAS COORDENADAS Y NOMBRES DE LOS SITIOS
+    let datos = [];
 
-    // DEFINIMOS EN LAS VARIABLES LA LATITUD DE LA POSICION DEL ARRAY 0 DEL JSON
-    latitud_mapa = data[0].latitud;
-    longitud_mapa = data[0].longitud;
+    // RECORREMOS EL SITIO, Y HACEMOS UN PUSH PARA GUARDARLOS EN EL ARRAY
+    for (let i = 0; i < data.length; i++) {
+        datos.push([data[i].latitud, data[i].longitud, data[i].nom]);
+    }
 
     // DECLARAMOS UNA VARIABLE DONDE DEFINIMOS EL ID DEL DIV PARA IMPRIMIT EL MAPA, Y 
     // AÑADIMOS LAS COORDENADAS CON EL setView (PRIMER PARAMETRO, LATITUD Y LONGITUD, SEGUNDO PARAMETRO EL SUM)
-    let myMap = L.map('myMap').setView([latitud_mapa, longitud_mapa], 13);
+    let myMap = L.map('myMap').setView([41.392109, 2.170840], 13);
 
     L.tileLayer(tilesProvider, {
         // ZOOM DEL MAPA DEL LEAFLET
         maxZoom: 18
-        //tileSize: 512
         // LO AÑADIMOS A LA VARIABLE DE myMaps
     }).addTo(myMap);
 
-    let marker = L.marker([latitud_mapa, longitud_mapa]).addTo(myMap);
+    datos.forEach((element) => {
+        const marker = L.marker([element[0], element[1]], {
+            title: element[2],
+        }).addTo(myMap);
+    });
 
     // DESHABILITAMOS EL ZOOM CON DBLCLICK
     myMap.doubleClickZoom.disable();
-    myMap.on('dblclick', function (e) {
-        // SELECCIONAMOS UN NUEVO MARCADOR CON EL marker, AÑADIMOS LA LATITUD Y LONGITUD 
-        // EN LA VARIABLE DE myMap (nuestro mapa creado anteriormente)
-        let new_marker = L.marker(e.latlng).addTo(myMap);
-        let markerpopup = L.popup({});
-        // AÑADIMOS EL POPUP Y SETTEAMOS LA LATITUD Y LONGITUD CON LA FUNCIÓN setLatLng
-        markerpopup.setLatLng(e.latlng);
-        // AÑADIMOS EL CONTENIDO DEL POPUP CON LA FUNCIÓN DE setContent
-        markerpopup.setContent("Popup");
-        // MARCAMOS EL ENLACE DEL POPUP
-        marker.bindPopup(markerpopup);
-    });
 }
